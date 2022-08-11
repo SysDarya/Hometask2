5ddb8ae955559404eb1b22435b1f6bc543afa7ac
Sys Darya <sysdaryaandreevna@gmail.com>
2022-08-11 15:11:37 +0300

    008 task

---
+++ 008/Program.cs
@@ -0,0 +1,16 @@
+﻿// Подсчитать сумму цифр в числе
+
+Console.WriteLine ("Введите число: ");
+int a = int.Parse (Console.ReadLine ()!);
+
+string str = a.ToString();
+int index = 0;
+int sum = 0;
+
+while (index < str.Length)
+{
+    sum = sum + int.Parse(str[index].ToString());
+    index++;
+}
+
+ Console.WriteLine ("Сумма цифр в числе равна " + sum + ".");
