aa18185a422e5d20036a7002c2d922ca4adfd353
Sys Darya <sysdaryaandreevna@gmail.com>
2022-08-11 14:46:46 +0300

    007 task

---
+++ 007/Program.cs
@@ -0,0 +1,7 @@
+﻿// Определить количество цифр в числе
+
+Console.WriteLine ("Введите число: ");
+int a = int.Parse (Console.ReadLine ()!);
+
+string str = a.ToString();
+ Console.WriteLine ("Количество цифр в числе равно " + str.Length + ".");
