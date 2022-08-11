65923c68f7d6bfc74bd6b777ed1f66226388f273
Sys Darya <sysdaryaandreevna@gmail.com>
2022-08-11 16:10:27 +0300

    010 task

---
+++ 010/Program.cs
@@ -0,0 +1,16 @@
+﻿// Показать кубы чисел, заканчивающихся на четную цифру
+
+Console.WriteLine ("Введите число: ");
+int a = int.Parse (Console.ReadLine ()!);
+
+int cube = 0;
+
+if (a%2 == 0)
+{
+    cube = a*a*a;
+    Console.WriteLine ("Число заканчивается на четную цифру и его куб равен " + cube + ".");
+}
+else
+{
+    Console.WriteLine ("К сожалению, число заканчивается на нечетную цифру :(");
+}
