7bb94e81aa9a71006f68e7766e57bdff1b597e42
Sys Darya <sysdaryaandreevna@gmail.com>
2022-08-11 13:03:35 +0300

    003 task

---
+++ 003/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задать номер четверти, показать диапазоны для возможных координат
+
+Console.WriteLine ("Введите четверть координатной плоскости: ");
+int q = int.Parse (Console.ReadLine ()!);
+
+if (q == 1)
+{
+    Console.WriteLine ("Диапазон координат для этой плоскости: x[0;	inf]; y[0; inf].");
+}
+else if (q == 2)
+{
+    Console.WriteLine ("Диапазон координат для этой плоскости: x[0;	negInf]; y[0; inf].");
+}
+else if (q == 3)
+{
+    Console.WriteLine ("Диапазон координат для этой плоскости: x[0;	negInf]; y[0; negInf].");
+}
+else if (q == 4)
+{
+    Console.WriteLine ("Диапазон координат для этой плоскости: x[0;	inf]; y[0; negInf].");
+}
+else
+{
+    Console.WriteLine ("У плоскости координат только 4 четверти :)");
+}
