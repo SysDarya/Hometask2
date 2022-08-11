22ea601137007553de8d1aaac056e9b18d9f395d
Sys Darya <sysdaryaandreevna@gmail.com>
2022-08-11 16:01:45 +0300

    009 task

---
+++ 009/Program.cs
@@ -0,0 +1,14 @@
+﻿// Написать программу вычисления произведения чисел от 1 до N
+
+Console.WriteLine ("Введите число-предел: ");
+int n = int.Parse (Console.ReadLine ()!);
+
+int count = 1;
+int mult = 1;
+
+while (count <= n)
+{
+    mult = mult * count;
+    count++;
+}
+Console.WriteLine ("Произведение чисел от 1 до " + n + " равно " + mult + ".");
