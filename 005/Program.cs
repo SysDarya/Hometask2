ebfc40b2ae3f6d0b3c27c019a7bccd0131535a64
Sys Darya <sysdaryaandreevna@gmail.com>
2022-08-11 14:04:48 +0300

    005 task

---
+++ 005/Program.cs
@@ -0,0 +1,14 @@
+﻿// Найти кубы чисел от 1 до N
+
+Console.WriteLine ("Введите число-предел: ");
+int n = int.Parse (Console.ReadLine ()!);
+
+int count = 1;
+int cube;
+
+while (count <= n)
+{
+    cube = count*count*count;
+    Console.WriteLine ("Куб числа " + count + " равен " + cube + ".");
+    count++;
+}
