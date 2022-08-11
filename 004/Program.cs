9079e74b9af08c54b6fe184db48acf3e04581d7d
Sys Darya <sysdaryaandreevna@gmail.com>
2022-08-11 13:52:54 +0300

    004 task

---
+++ 004/Program.cs
@@ -0,0 +1,24 @@
+﻿// Найти расстояние между точками в пространстве 2D/3D
+
+Console.WriteLine ("Координаты первой точки: ");
+int x = int.Parse (Console.ReadLine ()!);
+int y = int.Parse (Console.ReadLine ()!);
+int z = int.Parse (Console.ReadLine ()!);
+
+Console.WriteLine ("Координаты второй точки: ");
+int xs = int.Parse (Console.ReadLine ()!);
+int ys = int.Parse (Console.ReadLine ()!);
+int zs = int.Parse (Console.ReadLine ()!);
+
+double d;
+
+if (z == 0 && zs == 0)
+{
+    d = Math.Sqrt((xs-x)*(xs-x)+(ys-y)*(ys-y));
+    Console.WriteLine ("Расстояние равно " + d + ".");
+}
+else
+{
+    d = Math.Sqrt((xs-x)*(xs-x)+(ys-y)*(ys-y)+(zs-z)*(zs-z));
+    Console.WriteLine ("Расстояние равно " + d + ".");
+}
