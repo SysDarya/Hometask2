5fe3c413cffb417e1ddd79e8ae98d5fb063c671e
Sys Darya <sysdaryaandreevna@gmail.com>
2022-08-11 14:34:45 +0300

    006 task

---
+++ 006/Program.cs
@@ -0,0 +1,15 @@
+﻿// Найти сумму чисел от 1 до А
+
+Console.WriteLine ("Введите число-предел: ");
+int a = int.Parse (Console.ReadLine ()!);
+
+int count = 1;
+int sum = 0;
+
+while (count <= a)
+{
+    sum = sum + count;
+    
+    count++;
+}
+Console.WriteLine ("Cумма чисел от 1 до " + a + " равна " + sum + ".");
